33b2d75782a105da9c9b5a0c023d3386cfc14275
Sansone11 <sansonemirco@gmail.com>
2022-07-28 17:01:10 +0200

    pari e dispari-edit

--- pari-e-dispari/app.js
+++ pari-e-dispari/app.js
@@ -12,7 +12,21 @@ let userNumberChoise = parseInt(prompt('scegli un numero tra 1 e 5'));
 // creo un prompt per l'ùtente per scegliere pari o dispari
 let userChoise = prompt('scegli pari o dispari');
 // crea un generatore casuale di numeri da 1 a 5
-const cpuNumber = Math.floor(Math.random()*6);
+const cpuNumber = Math.floor(Math.random()*5)+1;
+console.log(cpuNumber)
+let somma = userNumberChoise + cpuNumber;
 
+let filter =IsNotUneven(somma) ;
 
-console.log(number);
+console.log(somma);
+// Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+function IsNotUneven(somma)
+{
+    if (somma %2 == 1  ){
+        console.log('dispari')
+    }
+    else
+    {
+        console.log('pari')
+    }
+}
