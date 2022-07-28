57a9a3fc555ecd76844e71b73b7bffc9e2c10dc1
Sansone11 <sansonemirco@gmail.com>
2022-07-28 16:22:03 +0200

    random-number-generator-edit

---
+++ pari-e-dispari/app.js
@@ -0,0 +1,18 @@
+console.log('JS ok');
+// L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+// Genero un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+// Sommiamo i due numeri
+// Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+// Dichiariamo chi ha vinto.
+
+
+
+// creo un prompt per l'utente che seleziona un valore tre 1 e 5
+let userNumberChoise = parseInt(prompt('scegli un numero tra 1 e 5'));
+// creo un prompt per l'ùtente per scegliere pari o dispari
+let userChoise = prompt('scegli pari o dispari');
+// crea un generatore casuale di numeri da 1 a 5
+const cpuNumber = Math.floor(Math.random()*6);
+
+
+console.log(number);
